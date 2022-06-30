ab12d89f03d53385ee002df698fcc83dce769ce5
unknown <https://github.com/Pazdziorko/Nowe.git>
2022-06-30 21:51:19 +0200

    pliki

--- kurs-java/Excercises/src/com/exercises/list/Array.java
+++ kurs-java/Excercises/src/com/exercises/list/Array.java
@@ -1,2 +1,15 @@
-package com.exercises.list;public class Array {
+package com.exercises.list;
+
+public class Array {
+    public static void main(String[] args) {
+
+        int table[] = new int[10];
+        for (int i = 101; i <= 110; i++){
+        int table2[] = new int[10];
+
+            System.out.println("Wartość = " + i);
+        }
+
+    }
+
 }
