5c14bf0dd023d4d1831fdfd9412a921c238f889f
unknown <https://github.com/Pazdziorko/Nowe.git>
2022-07-01 16:13:44 +0200

    pliki

--- kurs-java/Excercises/src/com/exercises/list/Array.java
+++ kurs-java/Excercises/src/com/exercises/list/Array.java
@@ -1,21 +1,57 @@
 package com.exercises.list;
 
+import java.util.Arrays;
+import java.util.SortedMap;
+
 public class Array {
     public static void main(String[] args) {
 
         int table[] = new int[10];
-        for (int i = 101; i <= 110; i++ ){
+        for (int i = 0; i < 10; i++) {
+            table[i] = 101 + i;
 
-            System.out.println("Wartość = " +  + i  );
+            System.out.println("Wartość table = " + table[i]);
 
         }
+        System.out.println();
+        int tableFirst[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            tableFirst[i] = (i + 1) * 10;
+            System.out.println("Wartość table_First = " + tableFirst[i]);
+        } System.out.println();
+        boolean tableSecond[] = new boolean[20];
 
-            System.out.println();
-
-        for (int i = 10; i<=100; i+=10 ){
-            System.out.println("Kolejna wartość = " + i);
-        }
-
+            for (int i = 0; i < 20; i += 2){
+                tableSecond[i] = true;
     }
 
+            for (int i = 0; i < 20; i ++){
+
+            System.out.println("Watrości table_second = " +  i + "\t" + tableSecond[i]);
+        }
+
+
+
+
+
+
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
