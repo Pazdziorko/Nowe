5b91a41f57bc8dfffc8ed38b03c7fd0eaf12a51e
unknown <https://github.com/Pazdziorko/Nowe.git>
2022-07-01 15:53:10 +0200

    pliki

--- kurs-java/Excercises/src/com/exercises/list/Array.java
+++ kurs-java/Excercises/src/com/exercises/list/Array.java
@@ -4,10 +4,16 @@ public class Array {
     public static void main(String[] args) {
 
         int table[] = new int[10];
-        for (int i = 101; i <= 110; i++){
-        int table2[] = new int[10];
+        for (int i = 101; i <= 110; i++ ){
 
-            System.out.println("Wartość = " + i);
+            System.out.println("Wartość = " +  + i  );
+
+        }
+
+            System.out.println();
+
+        for (int i = 10; i<=100; i+=10 ){
+            System.out.println("Kolejna wartość = " + i);
         }
 
     }
