f1cef15f28da3901f2c84a5866ffbbd44a06cdc3
Admin <pazdziorko91@gmail.com>
2022-07-09 12:02:23 +0200

    Init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+import exercise.User;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/exercise/User.java
@@ -0,0 +1,12 @@
+package exercise;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String name;
+    private String login;
+    private String password;
+}
