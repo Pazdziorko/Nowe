386efff17671a4538b1d08924573d0c1183b5308
Admin <pazdziorko91@gmail.com>
2022-07-20 13:19:45 +0200

    pliki

--- kurs-java/MySqlJdbcConnection/src/com/library/DriveManager.java
+++ kurs-java/MySqlJdbcConnection/src/com/library/DriveManager.java
@@ -1,2 +1,13 @@
-package com.library;public class DriveManager {
+package com.library;
+
+import java.sql.DriverManager;
+
+public class DriveManager {
+    protected String url = "jdbc:mysql://localhost:3306/student";
+    protected String user = "root";
+    protected String password = "pazdziorko1991";
+
+
+
+
 }
