e9509ff44f851c0def5a8d1261a09d765ccc83ee
Admin <pazdziorko91@gmail.com>
2022-08-02 15:27:56 +0200

    pliki

---
+++ src/main/java/listings/listings1/listing1_7/BlockDemo.java
@@ -0,0 +1,17 @@
+package listings.listings1.listing1_7;
+
+public class BlockDemo {
+    public static void main(String[] args) {
+
+        double i, j, d;
+        i = 5;
+        j= 10;
+
+        if (i != 0) {
+            System.out.println("i is not equal zero.");
+
+            d = j / i;
+            System.out.println("j / i = " + d);
+        }
+    }
+}
