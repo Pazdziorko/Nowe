48feff3ae81f3c73289f592093b6f9562202f086
Admin <pazdziorko91@gmail.com>
2022-08-02 15:14:55 +0200

    pliki

---
+++ src/main/java/listings/listings1/listing1_6/ForDemo.java
@@ -0,0 +1,11 @@
+package listings.listings1.listing1_6;
+
+public class ForDemo {
+    public static void main(String[] args) {
+        int count;
+
+        for (count = 0; count < 5; count = count + 1)
+            System.out.println("The value of the control variable count = " + count);
+        System.out.println("Loop is exeuted!");
+    }
+}
