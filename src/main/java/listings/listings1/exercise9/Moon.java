08b4cc71a52597bd60f6a3cab96c5c82d30ab616
Admin <pazdziorko91@gmail.com>
2022-08-03 10:54:29 +0200

    pliki

---
+++ src/main/java/listings/listings1/exercise9/Moon.java
@@ -0,0 +1,14 @@
+package listings.listings1.exercise9;
+
+public class Moon {
+    public static void main(String[] args) {
+        double earthweight;
+        double moonweight;
+
+        earthweight = 82;
+        moonweight = earthweight * 0.17;
+
+        System.out.println("My weight on the earth = " + earthweight + " kg.");
+        System.out.println("My weight on the moon = " + moonweight + " kg.");
+    }
+}
