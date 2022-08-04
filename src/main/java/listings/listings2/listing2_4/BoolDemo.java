3d4c64e928218681968ecdebdc1309169ed6db80
Admin <pazdziorko91@gmail.com>
2022-08-04 10:01:45 +0200

    pliki

---
+++ src/main/java/listings/listings2/listing2_4/BoolDemo.java
@@ -0,0 +1,26 @@
+package listings.listings2.listing2_4;
+
+public class BoolDemo {
+    public static void main(String[] args) {
+
+        boolean b;
+
+        b = true;
+        System.out.println("b = " + b);
+
+        b = false;
+        System.out.println("b = " + b);
+
+
+        if (b)
+        System.out.println("This is false");
+
+        b = true;
+        if (b)
+        System.out.println("This is true");
+
+        System.out.println("10 > 9 is " + (10 > 9));
+
+
+    }
+}
