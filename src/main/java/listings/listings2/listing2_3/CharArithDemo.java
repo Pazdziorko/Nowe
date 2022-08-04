8e29b374760a0403b0a1ec8d6bf8b6cf32040e1d
Admin <pazdziorko91@gmail.com>
2022-08-04 09:49:55 +0200

    pliki

---
+++ src/main/java/listings/listings2/listing2_3/CharArithDemo.java
@@ -0,0 +1,15 @@
+package listings.listings2.listing2_3;
+
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch = 'X';
+
+        System.out.println("ch = " + ch);
+
+        ch++;
+        System.out.println("ch = " + ch);
+
+        ch = 10121;
+        System.out.println("ch = " +ch);
+    }
+}
