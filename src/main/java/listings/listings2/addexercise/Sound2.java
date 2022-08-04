f2d5ca66c91d116a6a0552b1270d20808ded874e
Admin <pazdziorko91@gmail.com>
2022-08-04 10:20:59 +0200

    pliki

---
+++ src/main/java/listings/listings2/addexercise/Sound2.java
@@ -0,0 +1,17 @@
+package listings.listings2.addexercise;
+
+public class Sound2 {
+    public static void main(String[] args) {
+        double dist;
+        double time;
+
+        time = 120.25 / 2;
+        System.out.println("Time the sound one way = " + time + " seconds.");
+
+        dist = time * 330;
+        System.out.println("Distance from the object = " + dist + " meters.");
+
+
+
+    }
+}
---
+++ src/main/java/listings/listings2/listing2_5/Sound.java
@@ -0,0 +1,10 @@
+package listings.listings2.listing2_5;
+
+public class Sound {
+    public static void main(String[] args) {
+        double dist;
+
+        dist = 7.2 * 330;
+        System.out.println("The lightning hit " + dist + " meters from here.");
+    }
+}
