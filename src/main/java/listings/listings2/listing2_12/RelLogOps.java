e27528836935e7afdcd1f6a5d21a30511f66251e
Admin <pazdziorko91@gmail.com>
2022-09-22 15:36:13 +0200

    pliki

--- src/main/java/listings/listings2/listing2_12/RelLogOps.java
+++ src/main/java/listings/listings2/listing2_12/RelLogOps.java
@@ -1,2 +1,27 @@
-package listings.listings2.listing2_12;public class RelLogOps {
+package listings.listings2.listing2_12;
+// Demonstruje stosowanie operatorów relacyjnych i logicznych.
+public class RelLogOps {
+    public static void main(String[] args) {
+        int i, j;
+        boolean b1, b2;
+
+        i = 10;
+        j = 11;
+
+        if(i < j) System.out.println("i < j");
+        if(i <= j) System.out.println("i <= j");
+        if(i != j) System.out.println("i != j");
+        if(i == j) System.out.println("Wywołanie nie zostanie wykonane");
+        if(i >= j) System.out.println("Wywołanie nie zostanie wykonane");
+        if(i > j) System.out.println("Wywołanie nie zostanie wykonane");
+
+        b1 = true;
+        b2 = false;
+        System.out.println();
+
+        if(b1 & b2) System.out.println("Wywołanie nie zostanie wykonane");
+        if(!(b1 & b2)) System.out.println("! (b1 & b2) jest true");
+        if(b1 | b2) System.out.println("b1 | b2 jest true");
+        if(b1 ^ b2) System.out.println("b1 ^ b2 jest true");
+    }
 }
--- src/main/java/listings/listings2/listing2_13/SCops.java
+++ src/main/java/listings/listings2/listing2_13/SCops.java
@@ -1,2 +1,14 @@
-package listings.listings2.listing2_13;public class SCops {
+package listings.listings2.listing2_13;
+// Demonstruje stosowanie operatorów warunkowych.
+public class SCops {
+    public static void main(String[] args) {
+        int n, d, q;
+        n =10;
+        d = 2;
+        if(d != 0 && (n % d) ==0) System.out.println(d + " jest dzielnikiem " + n); // tutaj && zapobiega dzieleniu przez 0.
+
+        d = 0;
+        if(d != 0 && (n % d) ==0) System.out.println(d + " jest dzielnikiem " + n);
+        //if(d != 0 & (n % d) ==0) System.out.println(d + " jest dzielnikiem " +n); Operacja z operatorem & będzie błędna.
+    }
 }
